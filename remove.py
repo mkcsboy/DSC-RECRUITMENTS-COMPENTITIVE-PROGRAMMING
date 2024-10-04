987823c476c84302cee7af9d90924ba6c205fc78
MUKESH KUMAR <166938222+mkcsboy@users.noreply.github.com>
2024-10-05 00:23:44 +0530

    Delete remove.py

--- remove.py
+++
@@ -1,9 +0,0 @@
-def remove():
-    inlist=input().strip("[]")
-    l=inlist.split(',')
-    outlist=[]
-    for i in l:
-        if int(i) not in outlist:
-            outlist.append(int(i))
-    print(outlist)
-remove()
